e159ced173ff130c735fe9e7e8695d6632459c79
TenzinDann <1737239725@qq.com>
2026-03-20 19:24:27 -0700

    edit

--- App.tsx
+++ App.tsx
@@ -57,7 +57,7 @@ const App: React.FC = () => {
             <img
               src={logoSrc}
               alt="Site logo"
-              className="block h-[1.2em] w-auto object-contain shrink-0"
+              className="block h-[1.8em] w-auto object-contain shrink-0"
               style={{ filter: 'brightness(0)' }}
             />
             <span className={`font-serif font-bold tracking-wide transition-opacity leading-none ${scrolled ? 'opacity-100' : 'opacity-0 md:opacity-100'}`}>
